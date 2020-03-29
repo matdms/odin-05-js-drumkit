5ad4587eb84ebc769e1137bcc08b88844aeda5bf
matdms <mathieu.desmoulins@gmail.com>
2020-03-29 23:55:36 +0200

    1st page layout using CSS

--- script-md.js
+++ script-md.js
@@ -1,1 +1,27 @@
 //mon script de drum-kit
+
+function playSound(item) {
+    console.log(item);
+    
+}
+
+
+
+// eventListener pour les clics souris sur les touches 
+const touches = document.querySelectorAll('div.key');
+
+touches.forEach( (touche) => {
+    touche.addEventListener('click', (e) => {
+        playSound(touche.id);
+    });
+});
+
+
+// eventListener pour les appuis clavier
+const claviers = document.querySelectorAll('div.key');
+
+claviers.forEach( (clavier) => {
+    clavier.addEventListener('keypress', (e) => {
+        console.log(e.code);
+    });
+});

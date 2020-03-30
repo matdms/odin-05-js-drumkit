6d2b794ccb657a35e08a53a1e7c5979d2639e64b
matdms <mathieu.desmoulins@gmail.com>
2020-03-30 14:21:17 +0200

    Now working with keyboard input

--- script-md.js
+++ script-md.js
@@ -1,12 +1,10 @@
 //mon script de drum-kit
 
-
 // jouer un son depuis un clic
 function playSound(item) {
     console.log(item);
     new Audio('sounds/'+item+'.wav').play();   
 }
-
 
 // eventListener pour les clics souris sur les touches 
 const touches = document.querySelectorAll('div.key');
@@ -17,12 +15,40 @@ touches.forEach( (touche) => {
     });
 });
 
-
 // eventListener pour les appuis clavier
-const claviers = document.querySelectorAll('div.key');
-
-claviers.forEach( (clavier) => {
-    clavier.addEventListener('keypress', (e) => {
-        console.log(e.code);
-    });
+document.addEventListener('keydown', (e) => {
+    //console.log(e.code);
+    switch(e.code) {
+        case 'KeyA':
+            playSound('clap');
+            break;
+        case 'KeyS':
+            playSound('hihat');
+            break;
+        case 'KeyD':
+            playSound('kick');
+            break;
+        case 'KeyF':
+            playSound('openhat');
+            break;
+        case 'KeyG':
+            playSound('boom');
+            break;
+        case 'KeyH':
+            playSound('ride');
+            break;
+        case 'KeyJ':
+            playSound('snare');
+            break;
+        case 'KeyK':
+            playSound('tom');
+            break;
+        case 'KeyL':
+            playSound('tink');
+            break;
+        default:
+            console.log(e.code);
+    }
 });
+
+//end of script
